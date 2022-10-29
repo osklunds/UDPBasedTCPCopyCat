a030bc686afe8db0df72075e5a77500ff04ae092
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2022-10-29 08:28:22 +0200

    test_enc_flags_helper

--- src/segment/tests.rs
+++ src/segment/tests.rs
@@ -2,27 +2,18 @@ use super::*;
 
 #[test]
 fn test_enc_no_flags() {
-    // Arrange
-    let data = vec![7, 8, 9];
-    let seg = Segment::new(false, false, false, 123, 258, &data);
-
-    // Act
-    let enc = seg.encode();
-
-    // Assert
-    let len = 9 + data.len();
-    assert_eq!(enc.len(), len);
-    assert_eq!(enc[0], 0);
-    assert_eq!(&enc[1..5], vec![0, 0, 0, 123]);
-    assert_eq!(&enc[5..9], vec![0, 0, 1, 2]);
-    assert_eq!(&enc[9..len], &data);
+    test_enc_flags_helper(false, false, false, 0)
 }
 
 #[test]
 fn test_enc_all_flags() {
+    test_enc_flags_helper(true, true, true, 7)
+}
+
+fn test_enc_flags_helper(syn: bool, ack: bool, fin: bool, exp_byte: u8) {
     // Arrange
     let data = vec![7, 8, 9];
-    let seg = Segment::new(true, true, true, 123, 258, &data);
+    let seg = Segment::new(syn, ack, fin, 123, 258, &data);
 
     // Act
     let enc = seg.encode();
@@ -30,7 +21,7 @@ fn test_enc_all_flags() {
     // Assert
     let len = 9 + data.len();
     assert_eq!(enc.len(), len);
-    assert_eq!(enc[0], 7);
+    assert_eq!(enc[0], exp_byte);
     assert_eq!(&enc[1..5], vec![0, 0, 0, 123]);
     assert_eq!(&enc[5..9], vec![0, 0, 1, 2]);
     assert_eq!(&enc[9..len], &data);
