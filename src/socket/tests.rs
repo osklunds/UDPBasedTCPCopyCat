68734ff7582bdbcd6507dbe402e071bec1f587ea
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2022-11-05 08:58:27 +0100

    dummy commit

--- src/socket/tests.rs
+++ src/socket/tests.rs
@@ -16,7 +16,7 @@ fn test() {
 
     let client_socket = Stream::connect(server_addr);
     println!("{:?}", "connected");
-        println!("{:?}", "connected");
+    println!("{:?}", "connected");
 
     server_thread.join().unwrap();
 
