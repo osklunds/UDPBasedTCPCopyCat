1980126b8cb831a4c23e03691c800522a9183ba1
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2022-11-05 14:31:43 +0100

    client function

--- src/socket.rs
+++ src/socket.rs
@@ -33,6 +33,14 @@ struct ClientStream {
     write_tx: Sender<()>,
 }
 
+struct State {
+    seq_num: u32,
+    ack_num: u32,
+    udp_socket: UdpSocket,
+    read_tx: Sender<()>,
+    write_rx: Receiver<()>,
+}
+
 impl Listener {
     pub fn bind<A: ToSocketAddrs>(local_addr: A) -> Result<Listener> {
         match UdpSocket::bind(local_addr) {
@@ -68,45 +76,19 @@ impl Listener {
 }
 
 impl Stream {
-    pub fn connect<A: ToSocketAddrs>(to_peer_addr: A) -> Result<Stream> {
+    pub fn connect<A: ToSocketAddrs>(peer_addr: A) -> Result<Stream> {
         println!("connect called");
         let local_addr: SocketAddr = "0.0.0.0:0".parse().unwrap();
-        let peer_addr = to_peer_addr.to_socket_addrs().unwrap().last().unwrap();
+        //let peer_addr = to_peer_addr.to_socket_addrs().unwrap().last().unwrap();
 
-        let (_read_tx, read_rx) = mpsc::channel();
-        let (write_tx, _write_rx) = mpsc::channel();
+        let (read_tx, read_rx) = mpsc::channel();
+        let (write_tx, write_rx) = mpsc::channel();
 
         match UdpSocket::bind(local_addr) {
             Ok(udp_socket) => {
+                UdpSocket::connect(&udp_socket, peer_addr).unwrap();
                 thread::spawn(move || {
-                    let seq_num = rand::random();
-                    let syn =
-                        Segment::new(true, false, false, seq_num, 0, &vec![]);
-                    let encoded_syn = Segment::encode(&syn);
-
-                    udp_socket.send_to(&encoded_syn, peer_addr).unwrap();
-
-                    let mut buf = [0; 4096];
-                    let amt = udp_socket.recv(&mut buf).unwrap();
-
-                    let syn_ack = Segment::decode(&buf[0..amt]).unwrap();
-
-                    // TODO: Handle error instead
-                    assert_eq!(seq_num + 1, syn_ack.ack_num());
-
-                    let ack = Segment::new(
-                        false,
-                        true,
-                        false,
-                        seq_num + 1,
-                        syn_ack.seq_num() + 1,
-                        &vec![],
-                    );
-                    let encoded_ack = Segment::encode(&ack);
-
-                    udp_socket.send_to(&encoded_ack, peer_addr).unwrap();
-
-                    loop {}
+                    Self::client(udp_socket, read_tx, write_rx)
                 });
                 let stream = ClientStream { read_rx, write_tx };
                 Ok(Self::pack_client_stream(stream))
@@ -119,6 +101,61 @@ impl Stream {
         Stream {
             stream_inner: StreamInner::Client(client_stream),
         }
+    }
+
+    fn client(
+        udp_socket: UdpSocket,
+        read_tx: Sender<()>,
+        write_rx: Receiver<()>,
+    ) {
+        let (seq_num, ack_num) = Self::client_handshake(&udp_socket);
+
+        let mut state = State {
+            udp_socket,
+            seq_num,
+            ack_num,
+            read_tx,
+            write_rx,
+        };
+
+        loop {
+            state = Self::client_loop(state);
+        }
+    }
+
+    fn client_handshake(udp_socket: &UdpSocket) -> (u32, u32) {
+        let seq_num = rand::random();
+        let syn = Segment::new(true, false, false, seq_num, 0, &vec![]);
+        let encoded_syn = Segment::encode(&syn);
+
+        udp_socket.send(&encoded_syn).unwrap();
+
+        let mut buf = [0; 4096];
+        let amt = udp_socket.recv(&mut buf).unwrap();
+
+        let syn_ack = Segment::decode(&buf[0..amt]).unwrap();
+
+        let new_seq_num = seq_num + 1;
+        // TODO: Handle error instead
+        assert_eq!(new_seq_num, syn_ack.ack_num());
+        assert_eq!(true, syn_ack.syn());
+        assert_eq!(true, syn_ack.ack());
+        assert_eq!(false, syn_ack.fin());
+        assert_eq!(0, syn_ack.data().len());
+
+        let ack_num = syn_ack.seq_num() + 1;
+
+        let ack =
+            Segment::new(false, true, false, new_seq_num, ack_num, &vec![]);
+        let encoded_ack = Segment::encode(&ack);
+
+        udp_socket.send(&encoded_ack).unwrap();
+
+        (new_seq_num, ack_num)
+    }
+
+    fn client_loop(state: State) -> State {
+        state
     }
 
     fn accept<A: ToSocketAddrs>(
