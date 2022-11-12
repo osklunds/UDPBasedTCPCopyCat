82337bdf804dd57ce369cb134266a0daae25ef14
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2022-11-12 12:04:19 +0100

    Let connect reutnr State

--- src/socket/tests.rs
+++ src/socket/tests.rs
@@ -65,16 +65,14 @@ struct State {
 }
 
 #[test]
-fn test_client_connect() {
+fn test_connect() {
     let server_addr: SocketAddr = "127.0.0.1:6789".parse().unwrap();
     let server_udp_socket = UdpSocket::bind(server_addr).unwrap();
 
-    client_connect(&server_udp_socket);
+    connect(server_udp_socket);
 }
 
-fn client_connect(
-    server_udp_socket: &UdpSocket,
-) -> (Stream, SocketAddr, u32, u32) {
+fn connect(server_udp_socket: UdpSocket) -> State {
     // Connect
     let server_addr = server_udp_socket.local_addr().unwrap();
     let client_stream = Stream::connect(&server_addr).unwrap();
@@ -95,7 +93,13 @@ fn client_connect(
     server_seq_num += 1;
     assert_eq!(server_seq_num, ack.ack_num());
 
-    (client_stream, client_addr, server_seq_num, client_seq_num)
+    State {
+        udp_socket: server_udp_socket,
+        stream: client_stream,
+        peer_addr: client_addr,
+        seq_num: server_seq_num,
+        ack_num: client_seq_num,
+    }
 }
 
 #[test]
@@ -105,16 +109,7 @@ fn test_client_read_once() {
     let server_udp_socket = UdpSocket::bind(server_addr).unwrap();
 
     // Connect the client
-    let (client_stream, client_addr, server_num, client_num) =
-        client_connect(&server_udp_socket);
-
-    let mut state = State {
-        udp_socket: server_udp_socket,
-        stream: client_stream,
-        peer_addr: client_addr,
-        seq_num: server_num,
-        ack_num: client_num,
-    };
+    let mut state = connect(server_udp_socket);
 
     read(&mut state, "some data".as_bytes());
 }
