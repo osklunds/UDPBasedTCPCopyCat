3e85dde951750645be6bcfe18bc336d1a9397fdc
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2023-01-05 13:42:11 +0100

    Put global vars in mutexes

--- src/controllable_timer.rs
+++ src/controllable_timer.rs
@@ -8,13 +8,14 @@ mod tests;
 use async_channel::{Receiver, Sender};
 use async_std::future;
 use futures::executor::block_on;
+use futures::lock::{Mutex, MutexGuard};
 use std::time::Duration;
 
 struct Channels {
-    sleep_called_tx: Sender<()>,
-    sleep_called_rx: Receiver<()>,
-    let_sleep_return_tx: Sender<()>,
-    let_sleep_return_rx: Receiver<()>,
+    sleep_called_tx: Mutex<Sender<()>>,
+    sleep_called_rx: Mutex<Receiver<()>>,
+    let_sleep_return_tx: Mutex<Sender<()>>,
+    let_sleep_return_rx: Mutex<Receiver<()>>,
 }
 
 static mut CHANNELS: Option<Channels> = None;
@@ -26,10 +27,10 @@ pub fn initialize() {
     let (let_sleep_return_tx, let_sleep_return_rx) = async_channel::bounded(1);
 
     let channels = Channels {
-        sleep_called_tx,
-        sleep_called_rx,
-        let_sleep_return_tx,
-        let_sleep_return_rx,
+        sleep_called_tx: Mutex::new(sleep_called_tx),
+        sleep_called_rx: Mutex::new(sleep_called_rx),
+        let_sleep_return_tx: Mutex::new(let_sleep_return_tx),
+        let_sleep_return_rx: Mutex::new(let_sleep_return_rx),
     };
 
     unsafe { CHANNELS = Some(channels) }
@@ -42,10 +43,13 @@ unsafe fn get_channels() -> &'static Channels {
 pub fn wait_for_sleep_called() {
     block_on(async {
         unsafe {
-            let recv_sleep_called = get_channels().sleep_called_rx.recv();
+            let sleep_called_rx = get_channels().sleep_called_rx.lock().await;
+            let recv_sleep_called = sleep_called_rx.recv();
+
             let duration = Duration::from_millis(10);
             let timeout_result =
                 future::timeout(duration, recv_sleep_called).await;
+
             let recv_result =
                 timeout_result.expect("Timeout waiting for sleep to be called");
             recv_result.expect("Error receiving from sleep_called_rx");
@@ -55,10 +59,14 @@ pub fn wait_for_sleep_called() {
 
 pub fn let_sleep_return() {
     unsafe {
-        get_channels()
-            .let_sleep_return_tx
-            .try_send(())
-            .expect("Error sending on let_sleep_return_tx");
+        block_on(async {
+            get_channels()
+                .let_sleep_return_tx
+                .lock()
+                .await
+                .try_send(())
+                .expect("Error sending on let_sleep_return_tx");
+        });
     }
 }
 
@@ -68,11 +76,15 @@ pub async fn sleep() {
     unsafe {
         get_channels()
             .sleep_called_tx
+            .lock()
+            .await
             .try_send(())
             .expect("Error sending on sleep_called_tx");
 
         get_channels()
             .let_sleep_return_rx
+            .lock()
+            .await
             .recv()
             .await
             .expect("Error receiving on let_sleep_return_rx")
