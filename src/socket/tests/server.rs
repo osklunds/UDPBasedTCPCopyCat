e0614635bbeaf2ab06cf2558e3f9f2fa3837536d
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2024-08-20 14:30:39 +0200

    Add start of reads and writes server test

--- src/socket/tests/server.rs
+++ src/socket/tests/server.rs
@@ -318,8 +318,124 @@ fn mf_explicit_sequence_numbers_two_clients() {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+// Read/write test cases
+////////////////////////////////////////////////////////////////////////////////
+
+#[test]
+fn mf_reads_and_writes() {
+    let listener_state = uut_listen();
+    let mut stream_state = uut_accept(&listener_state);
+
+    uut_write_with_tc_ack(&mut stream_state, b"some data");
+    uut_write_with_tc_ack(&mut stream_state, b"other data this time");
+}
+
+////////////////////////////////////////////////////////////////////////////////
 // Helper functions
 ////////////////////////////////////////////////////////////////////////////////
+
+struct ListenerState {
+    listener: Listener,
+    uut_addr: SocketAddr,
+}
+
+fn uut_listen() -> ListenerState {
+    let initial_addr: SocketAddr = "127.0.0.1:0".parse().unwrap();
+    let listener = Listener::bind(initial_addr).unwrap();
+    let uut_addr = listener.local_addr().unwrap();
+
+    ListenerState {
+        listener,
+        uut_addr
+    }
+}
+
+struct StreamState {
+    tc_socket: UdpSocket,
+    uut_stream: Option<Stream>,
+    uut_addr: SocketAddr,
+    send_next: u32,
+    receive_next: u32,
+}
+
+fn uut_accept(listener_state: &ListenerState) -> StreamState {
+    let initial_addr: SocketAddr = "127.0.0.1:0".parse().unwrap();
+    let uut_addr = listener_state.uut_addr;
+
+    let connect_client_thread = thread::spawn(move || {
+        let client_socket = UdpSocket::bind(initial_addr).unwrap();
+        let client_addr = client_socket.local_addr().unwrap();
+        assert_ne!(client_addr, uut_addr);
+
+        client_socket
+            .set_read_timeout(Some(Duration::from_millis(2)))
+            .unwrap();
+
+        UdpSocket::connect(&client_socket, uut_addr).unwrap();
+
+        // For seq num, always from UUT's POV
+        let mut receive_next = rand::random();
+
+        // Send SYN
+        let syn = Segment::new_empty(Syn, receive_next, 0);
+        send_segment_to(&client_socket, uut_addr, &syn);
+        receive_next += 1;
+
+        // Receive SYN-ACK
+        let syn_ack = recv_segment_from(&client_socket, uut_addr);
+        assert_eq!(SynAck, syn_ack.kind());
+        assert_eq!(receive_next, syn_ack.ack_num());
+        assert_eq!(b"", syn_ack.data());
+        let send_next = syn_ack.seq_num() + 1;
+
+        // Send ACK
+        let ack = Segment::new_empty(Ack, receive_next, send_next);
+        send_segment_to(&client_socket, uut_addr, &ack);
+
+        (client_socket, send_next, receive_next)
+    });
+
+    let (uut_stream, client_addr) = listener_state.listener.accept().unwrap();
+    let (client_socket, send_next, receive_next) = connect_client_thread.join().unwrap();
+    assert_eq!(client_addr, client_socket.local_addr().unwrap());
+
+    StreamState {
+        tc_socket: client_socket,
+        uut_stream: Some(uut_stream),
+        uut_addr: listener_state.uut_addr,
+        send_next,
+        receive_next,
+    }
+}
+
+fn uut_write_with_tc_ack(stream_state: &mut StreamState, data: &[u8]) {
+    // Send from the uut
+    let written_len = uut_stream(stream_state).write(&data).unwrap();
+    let len = data.len();
+    assert_eq!(len, written_len);
+
+    // Recv from the tc
+    let exp_seg = Segment::new(Ack, stream_state.send_next, stream_state.receive_next, &data);
+    recv__expect_segment(&stream_state, &exp_seg);
+    stream_state.send_next += len as u32;
+
+    // Send ack from the tc
+    let send_ack = Segment::new_empty(Ack, stream_state.receive_next, stream_state.send_next);
+    send_segment(&stream_state, &send_ack);
+}
+
+fn uut_stream(stream_state: &mut StreamState) -> &mut Stream {
+    stream_state.uut_stream.as_mut().unwrap()
+}
+
+fn recv__expect_segment(stream_state: &StreamState, exp_seg: &Segment) {
+    let recv_seg = recv_segment(stream_state);
+    assert_eq!(exp_seg, &recv_seg);
+}
+
+fn recv_segment(stream_state: &StreamState) -> Segment {
+    recv_segment_from(&stream_state.tc_socket, stream_state.uut_addr)
+}
 
 fn recv_segment_from(
     client_socket: &UdpSocket,
@@ -334,6 +450,10 @@ fn recv_segment_with_addr(client_socket: &UdpSocket) -> (Segment, SocketAddr) {
     let mut buf = [0; 4096];
     let (amt, recv_addr) = client_socket.recv_from(&mut buf).unwrap();
     (Segment::decode(&buf[0..amt]).unwrap(), recv_addr)
+}
+
+fn send_segment(stream_state: &StreamState, segment: &Segment) {
+    send_segment_to(&stream_state.tc_socket, stream_state.uut_addr, segment);
 }
 
 fn send_segment_to(
