06f587b96cb1bbea05f442db5d92af9a063dff10
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2024-08-22 11:25:57 +0200

    Add more Gate tests

--- src/gate/tests.rs
+++ src/gate/tests.rs
@@ -2,6 +2,7 @@
 use super::*;
 
 use std::time::Duration;
+use std::thread;
 
 use async_std::future;
 use async_std::future::TimeoutError;
@@ -16,6 +17,15 @@ fn can_pass_after_creation() {
 }
 
 #[test]
+fn can_open_after_creation() {
+    let (controller, user) = new();
+
+    controller.open();
+
+    assert_can_pass(user);
+}
+
+#[test]
 fn can_pass_multiple_times() {
     let (_controller, mut user) = new();
 
@@ -25,7 +35,7 @@ fn can_pass_multiple_times() {
 }
 
 #[test]
-fn cannot_pass_after_close() {
+fn cannot_pass_after_single_close() {
     let (controller, mut user) = new();
 
     user = assert_can_pass(user);
@@ -33,6 +43,75 @@ fn cannot_pass_after_close() {
     controller.close();
 
     assert_cannot_pass(user);
+}
+
+#[test]
+fn cannot_pass_after_multiple_closes() {
+    let (controller, mut user) = new();
+
+    user = assert_can_pass(user);
+
+    controller.close();
+    controller.close();
+    controller.close();
+
+    assert_cannot_pass(user);
+}
+
+#[test]
+fn can_pass_after_single_open() {
+    let (controller, mut user) = new();
+
+    user = assert_can_pass(user);
+
+    controller.close();
+    controller.open();
+
+    user = assert_can_pass(user);
+    user = assert_can_pass(user);
+    assert_can_pass(user);
+}
+
+#[test]
+fn can_pass_after_multiple_opens() {
+    let (controller, mut user) = new();
+
+    user = assert_can_pass(user);
+
+    controller.close();
+
+    controller.open();
+    controller.open();
+    controller.open();
+
+    user = assert_can_pass(user);
+    user = assert_can_pass(user);
+    assert_can_pass(user);
+}
+
+#[test]
+fn open_while_trying_to_pass() {
+    let (controller, mut user) = new();
+
+    user = assert_can_pass(user);
+
+    controller.close();
+
+    let passer_thread = thread::spawn(move || {
+        block_on(user.pass())
+    });
+
+    // Make user had a chance to call pass and get stuck
+    thread::sleep(Duration::from_millis(5));
+
+    // Make sure user hasn't passed through too early
+    assert!(!passer_thread.is_finished());
+
+    controller.open();
+
+    user = passer_thread.join().unwrap();
+
+    assert_can_pass(user);
 }
 
 fn assert_can_pass(user: GateUser) -> GateUser {
